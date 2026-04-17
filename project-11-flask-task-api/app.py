2ed08bbf2e03d556af27377bad5bc58805381043
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-04-17 11:57:52 +0300

    Project-11-flask-task-api - app.py

--- project-11-flask-task-api/app.py
+++ project-11-flask-task-api/app.py
@@ -5,28 +5,20 @@ import os
 app = Flask(__name__)
 FILE_NAME = "tasks.json"
 
-
-# Load tasks
 def load_tasks():
     if os.path.exists(FILE_NAME):
         with open(FILE_NAME, "r") as f:
             return json.load(f)
     return []
 
-
-# Save tasks
 def save_tasks(tasks):
     with open(FILE_NAME, "w") as f:
         json.dump(tasks, f, indent=4)
 
-
-# Get all tasks
 @app.route("/tasks", methods=["GET"])
 def get_tasks():
     return jsonify(load_tasks())
 
-
-# Add new task
 @app.route("/tasks", methods=["POST"])
 def add_task():
     data = request.get_json()
@@ -43,8 +35,6 @@ def add_task():
 
     return jsonify(new_task), 201
 
-
-# Update task
 @app.route("/tasks/<int:task_id>", methods=["PUT"])
 def update_task(task_id):
     tasks = load_tasks()
@@ -55,18 +45,14 @@ def update_task(task_id):
             save_tasks(tasks)
             return jsonify(task)
 
-    return jsonify({"error": "Task not found"}), 404
+    return jsonify({"error": "Not found"}), 404
 
-
-# Delete task
 @app.route("/tasks/<int:task_id>", methods=["DELETE"])
 def delete_task(task_id):
     tasks = load_tasks()
     tasks = [t for t in tasks if t["id"] != task_id]
     save_tasks(tasks)
-
-    return jsonify({"message": "Task deleted"})
-
+    return jsonify({"message": "Deleted"})
 
 if __name__ == "__main__":
     app.run(debug=True)
